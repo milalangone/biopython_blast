2e9bbade4f947f671ec8a2d04b0b2ec350935f63
Brisa Rojas <rojaasbrisa@gmail.com>
2024-04-16 17:57:53 -0300

    check env is ok before pipeline exec + refactor  and comment

--- pipeline.sh
+++ pipeline.sh
@@ -1,12 +1,21 @@
-touch Log.log
-chmod 777 Log.log
+# If environment is not prepared, prepare it or stop execution if needed
+source prepare_env.sh
+if [ $? -ne 0 ]; then
+  echo "Error in prepare_env.sh - Something could not be installed/upgraded - Check before running this script again."
+  exit 1
+fi
 
+# Create log file and give permissions
+LOG_FILE="log.log"
+touch $LOG_FILE
+chmod 777 $LOG_FILE
+
+# Run the pipeline with unique timestamp for each execution
 fecha_actual=$(date +'%d/%m/%y %H:%M')
+echo ">>$fecha_actual" >> $LOG_FILE
 
-echo ">>$fecha_actual" >> Log.log
-
-python3 Ex1.py -s transcript_3.gb >> "Log.log"
-python3 best_rf.py -t "fasta_output.fasta" >> "Log.log"
+python3 Ex1.py -s transcript_3.gb >> $LOG_FILE
+python3 best_rf.py -t "fasta_output.fasta" >> $LOG_FILE
 #python3 Ex2.py -f "fasta_longest.fasta"
-python3 Ex3.py >> "Log.log"
+python3 Ex3.py >> $LOG_FILE
 
